2435f9bba3abbded35766aa652ffa61268e6584c
Malcolm VanOrder <mvanorder1390@gmail.com>
2018-03-25 22:11:40 -0400

    Start quantities.
    Signed-off-by: Malcolm VanOrder <mvanorder1390@gmail.com>

--- recipes/models.py
+++ recipes/models.py
@@ -9,8 +9,24 @@ class Ingredient(models.Model):
         return self.name
 
 
+class QuantityType(models.Model):
+    """Type of quantity.
+    """
+    name = models.CharField(max_length=15)
+
+
+class Quantity(models.Model):
+    name = models.CharField(max_length=15)
+    scale = models.CharField(max_length=15)
+    quantity_type = models.ForeignKey(QuantityType)
+
+    def __str__(self):
+        return str(self.name)
+
+
 class Recipe(models.Model):
     name = models.CharField(max_length=30)
+    description = models.TextField(blank=True, null=True)
     directions = models.TextField()
     prep_time = models.DurationField(blank=True, null=True)
     cook_time = models.DurationField(blank=True, null=True)
@@ -30,7 +46,8 @@ class Recipe(models.Model):
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe)
     ingredient = models.ForeignKey(Ingredient)
-    quantity_amount = models.TextField(max_length=8)
+    quantity_amount = models.CharField(max_length=8)
+    quantity = models.ForeignKey(Quantity, blank=True, null=True)
 
     @property
     def name(self):
